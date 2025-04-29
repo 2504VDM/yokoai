8e9829ff5a6fb142f662d3fb199a725e5dd56efb
Dennis van der Meulen <2504sports@gmail.com>
2025-04-29 14:35:00 +0200

    Switch to uvicorn ASGI server

--- config/asgi.py
+++ config/asgi.py
@@ -1,15 +1,17 @@
 """
-ASGI config for config project.
-
+ASGI config for project.
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import sys
 
 from django.core.asgi import get_asgi_application
+
+# Add the project directory to the Python path
+current_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+if current_path not in sys.path:
+    sys.path.append(current_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
