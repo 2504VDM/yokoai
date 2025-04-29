8600b5ec24300c85254dbbd31331b5fc7bde0298
Dennis van der Meulen <2504sports@gmail.com>
2025-04-29 14:46:53 +0200

    Update database URL and add djangorestframework

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
 import sys
+import django
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent
 
 # Add the project directory to the Python path
-current_path = os.path.abspath(os.path.dirname(__file__))
-if current_path not in sys.path:
-    sys.path.append(current_path)
+if str(BASE_DIR) not in sys.path:
+    sys.path.append(str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Initialize Django
+django.setup()
 
 from django.core.wsgi import get_wsgi_application
 app = get_wsgi_application() 
